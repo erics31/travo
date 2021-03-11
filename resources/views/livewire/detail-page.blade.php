059d31fc5b23388c9d1a13dd5ef53709ed079e74
Your Name <you@example.com>
2021-03-11 12:31:13 -0800

    details-page

--- resources/views/livewire/detail-page.blade.php
+++ resources/views/livewire/detail-page.blade.php
@@ -1,12 +1,226 @@
 <style>
    .color{
-       /* background:red; */
+       /* background:red;*/
    }
+
+   
+.hero-wrap {
+  width: 100%;
+  position: relative; }
+  .hero-wrap .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    content: '';
+    opacity: .6;
+    width: 50%;
+    background: #82ae46; }
+  .hero-wrap .slider-text {
+    color: #fff;
+    position: relative; }
+    .hero-wrap .slider-text .breadcrumbs {
+      text-transform: uppercase;
+      font-size: 12px;
+      letter-spacing: 3px;
+      margin-bottom: 0;
+      z-index: 99;
+      font-weight: 300; }
+      .hero-wrap .slider-text .breadcrumbs span {
+        color: white; }
+        .hero-wrap .slider-text .breadcrumbs span a {
+          color: #fff; }
+    .hero-wrap .slider-text .bread {
+      font-weight: 800;
+      color: #fff;
+      font-size: 30px;
+      font-family: "Poppins", Arial, sans-serif;
+      letter-spacing: 3px;
+      text-transform: uppercase; }
+    .hero-wrap .slider-text .btn-primary {
+      border: 1px solid rgba(255, 255, 255, 0.4);
+      -webkit-border-radius: 30px;
+      -moz-border-radius: 30px;
+      -ms-border-radius: 30px;
+      border-radius: 30px; }
+      .hero-wrap .slider-text .btn-primary:hover, .hero-wrap .slider-text .btn-primary:focus {
+        background: #fff !important;
+        color: #000000; }
+  .hero-wrap.hero-bread {
+   }
+
+
+
+
+
+   .blog_details {
+  padding: 20px 10 0px 10px;
+  box-shadow: 0px 10px 20px 0px rgba(221, 221, 221, 0.3);
+  background-color:white;
+
+}
+..blog_details a h5 {
+margin-left:20px;
+
+}
+
+.blog__item__text {
+margin-left:20px;
+
+}
+
+.blog__item__text h6 {
+  margin-bottom: 6px;
+}
+
+.blog__item__text h6 a {
+  color: #111111;
+  font-weight: 600;
+  line-height: 21px;
+}
+
+.blog__item__text ul li {
+  font-size: 12px;
+  color: #888888;
+  display: inline-block;
+  list-style: none;
+  margin-right: 25px;
+  position: relative;
+}
+
+.blog__item__text ul li span {
+  color: #111111;
+}
+
+.blog__item__text ul li:after {
+  position: absolute;
+  right: -17px;
+  top: 0px;
+  content: "|";
+}
+
+.blog__item__text ul li:last-child {
+  margin-right: 0;
+}
+
+.blog__item__text ul li:last-child:after {
+  display: none;
+}
+
+
+
+
 </style>
 <div class="banner color">
-    <div class="banner-img"><img src="images/banner.png" alt=""> </div>
+    <div class="banner-img"><img src="img/appartement.jpg" alt=""style="height:300px; object-fit: cover;"> </div>
 
     @livewire('search-view')
     <!--searchbar-->
-    <!-- put your code for detail page here -->
+    <!-- put your code for detail page here -->  
+
+
 </div>
+
+<div>
+          <iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
+        </div>
+
+     
+        <section class="hero-section"style="background-color:lavender " >
+      <div class="container">
+
+  <div class="row">
+    <div class="col-lg-4 col-md-6 col-sm-10"style="margin-top: 32px;">
+       <div class="single-services mb-200">
+                            
+    <div>
+   <img src="img/apart/7.jpg" >
+       
+     </div>
+   
+</div>
+                          
+      <div class="blog_details">
+      <div class="blog__item__text">
+          <a class="d-inline-block" href="single-blog.html">
+       <h5>Painthouse - direct oceanfront</h5>
+                                </a>       
+                  <ul>
+                  <li>France, Paris;</li>
+                  <li><span>100m from louvre museum</span></li>
+                   <p class=" para">$5.00/hour</p>
+
+                            </ul>
+                        </div>
+
+                            </div>
+
+                        
+                        </div>
+
+
+                        <div class="col-lg-4 col-md-6 col-sm-10"style="margin-top: 32px;">
+       <div class="single-services mb-200">
+                            
+    <div>
+   <img src="img/apart/7.jpg" >
+       
+     </div>
+   
+</div>
+                          
+      <div class="blog_details">
+    
+                             
+          <div class="blog__item__text">
+          <a class="d-inline-block" href="single-blog.html">
+       <h5>Painthouse - direct oceanfront</h5>
+                                </a> 
+                           
+                  <ul>
+                  <li>France, Paris;</li>
+                  <li><span>100m from louvre museum</span></li>
+                   <p class=" para">$5.00/hour</p>
+
+                            </ul>
+                        </div>
+
+                            </div>
+
+                        
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 col-sm-10"style="margin-top: 32px;">
+       <div class="single-services mb-200">
+                            
+    <div>
+   <img src="img/apart/7.jpg" >
+       
+     </div>
+   
+</div>
+                          
+      <div class="blog_details">                   
+          <div class="blog__item__text">
+          <a class="d-inline-block" href="single-blog.html">
+       <h5>Painthouse - direct oceanfront</h5>
+                                </a> 
+                           
+                  <ul>
+                  <li>France, Paris;</li>
+                  <li><span>100m from louvre museum</span></li>
+                   <p class=" para">$5.00/hour</p>
+
+                            </ul>
+                        </div>
+
+                            </div>
+
+                        
+                        </div>
+                    </div>
+
+
+</div>
+</section>      
--- resources/views/livewire/search-view.blade.php
+++ resources/views/livewire/search-view.blade.php
@@ -1,8 +1,22 @@
+<style>
+    .sr-inner {
+     padding: 15px;
+        margin: 0 auto;
+        border-radius: 20px;
+        background: transparent;
+    }
+</style>
 <div>
     <div class="searchbar">
-        <div class="sr-head">
-            <p>Find Your Perfect Space </p>
-            <p>15 City, 400 Places</p>
+        <div  class="hero-wrap hero-bread">
+        <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <p class="breadcrumbs"> <span>Enjoy all</span></p>
+            <h1 class="mb-0 bread">Your perfect appartements</h1>
+          </div>
+        </div>
+      </div>
         </div>
 
         <div class="sr-inner">
